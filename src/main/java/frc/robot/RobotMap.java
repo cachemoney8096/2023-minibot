5e701db78db95d98441212d7b3bb7cdbed7c44ec
Sid <gujjasidharth@gmail.com>
2023-08-16 20:24:53 -0500

    Updated WPILib, Added SparkMaxes to Grabber

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -2,21 +2,21 @@ package frc.robot;
 
 public class RobotMap {
     /** Driving Motor CAN IDs  */
-    public int FRONT_LEFT_DRIVE_MOTOR_CAN_ID = 1,
+    public static int FRONT_LEFT_DRIVE_MOTOR_CAN_ID = 1,
      FRONT_RIGHT_DRIVE_MOTOR_CAN_ID = 3,
      BACK_LEFT_DRIVE_MOTOR_CAN_ID = 2,
-     BACK_RIGHT_DRIVE_MOTOR = 4;
+     BACK_RIGHT_DRIVE_MOTOR_CAN_ID = 4;
 
     /** Steering Motor CAN IDs */
-    public int FRONT_LEFT_STEERING_MOTOR_CAN_ID = 5,
+    public static int FRONT_LEFT_STEERING_MOTOR_CAN_ID = 5,
      FRONT_RIGHT_STEERING_MOTOR_CAN_ID = 6,
      BACK_LEFT_STEERING_MOTOR_CAN_ID = 7,
      BACK_RIGHT_STEERING_MOTOR_CAN_ID = 8;
 
     /** Arm Pivot Motor CAN ID */
-    public int ARM_PIVOT_MOTOR_CAN_ID = 9;
+    public static int ARM_PIVOT_MOTOR_CAN_ID = 9;
 
     /** Intake Roller Motor CAN IDs */
-    public int LEFT_INTAKE_ROLLER_MOTOR_CAN_ID = 10, RIGHT_INTAKE_ROLLER_MOTOR_CAN_ID = 11;
+    public static int LEFT_INTAKE_ROLLER_MOTOR_CAN_ID = 10, RIGHT_INTAKE_ROLLER_MOTOR_CAN_ID = 11;
 
 }
