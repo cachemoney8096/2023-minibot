c969424b367e0051194b0b464a8caef1ae5a0f4e
Sid <79818750+Panda8188@users.noreply.github.com>
2023-08-18 15:33:33 -0500

    Added an outtake function for grabber

--- src/main/java/frc/robot/subsystems/Grabber.java
+++ src/main/java/frc/robot/subsystems/Grabber.java
@@ -1,22 +1,13 @@
 package frc.robot.subsystems;
-
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
-import frc.robot.RobotMap;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Grabber extends SubsystemBase {
-    private CANSparkMax FRONT_LEFT_DRIVE_MOTOR = new CANSparkMax(RobotMap.FRONT_LEFT_DRIVE_MOTOR_CAN_ID, MotorType.kBrushless);
-    private CANSparkMax FRONT_RIGHT_DRIVE_MOTOR = new CANSparkMax(RobotMap.FRONT_RIGHT_DRIVE_MOTOR_CAN_ID, MotorType.kBrushless);
-    private CANSparkMax BACK_LEFT_DRIVE_MOTOR = new CANSparkMax(RobotMap.BACK_LEFT_DRIVE_MOTOR_CAN_ID, MotorType.kBrushless);
-    private CANSparkMax BACK_RIGHT_DRIVE_MOTOR = new CANSparkMax(RobotMap.BACK_RIGHT_DRIVE_MOTOR_CAN_ID, MotorType.kBrushless);
-
     public void intake() {};
     public void scoreHigh() {};
     public void scoreMid() {};
     public void scoreLow() {};
+    public void outtake() {};
+
 
     @Override
     public void periodic() {};
