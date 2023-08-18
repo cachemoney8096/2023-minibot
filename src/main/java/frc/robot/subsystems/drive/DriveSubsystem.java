2433ae912e22e3e5be6f3d418f2f15c1982eaf91
Sid <79818750+Panda8188@users.noreply.github.com>
2023-08-17 23:29:55 -0500

    Moved Drive Motors to DriveSubsystem

--- src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
@@ -3,13 +3,21 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems.drive;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import frc.robot.RobotMap;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveSubsystem extends SubsystemBase {
-  public DriveSubsystem() {
-    
-  }
+  private CANSparkMax FRONT_LEFT_DRIVE_MOTOR = new CANSparkMax(RobotMap.FRONT_LEFT_DRIVE_MOTOR_CAN_ID, MotorType.kBrushless);
+  private CANSparkMax FRONT_RIGHT_DRIVE_MOTOR = new CANSparkMax(RobotMap.FRONT_RIGHT_DRIVE_MOTOR_CAN_ID, MotorType.kBrushless);
+  
+  private CANSparkMax BACK_LEFT_DRIVE_MOTOR = new CANSparkMax(RobotMap.BACK_LEFT_DRIVE_MOTOR_CAN_ID, MotorType.kBrushless);
+  private CANSparkMax BACK_RIGHT_DRIVE_MOTOR = new CANSparkMax(RobotMap.BACK_RIGHT_DRIVE_MOTOR_CAN_ID, MotorType.kBrushless);
+
+  public DriveSubsystem() {}
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
