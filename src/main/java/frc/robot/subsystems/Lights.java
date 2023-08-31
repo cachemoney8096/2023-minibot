86e7b18e1423619ff1bf650e61fb8aa60b37fb7d
Pxndah <gujjasidharth@gmail.com>
2023-08-31 11:52:33 -0500

    Fixed Lights Subsystem
    * Fixed RGB Values
    * Changed Light Code NO_TAG to larson green animation, blinking every half a second
    * Moved party mode initialization to setLEDs
    * Added comments

--- src/main/java/frc/robot/subsystems/Lights.java
+++ src/main/java/frc/robot/subsystems/Lights.java
@@ -14,15 +14,23 @@ public class Lights extends SubsystemBase {
   private CANdle candle = new CANdle(RobotMap.CANDLE_CAN_ID);
   private CANdleConfiguration config = new CANdleConfiguration();
   private LightCode currentLightStatus = LightCode.OFF;
+  
+  /** Timer for the Blinking LED */
+  private int blinkingTimer = 0;
+  /** When the blinkingTimer reaches 10 (blinkingPeriod), the LED is toggled */
+  private int blinkingPeriod = 10;
+  /** If LEDs are currently blinking, then True would mean color is currently showing */
+  private boolean blinkingColorOn = false;
+
 
   public enum LightCode {
     GAME_OBJECT, // green
     READY_TO_SCORE, // blue
-    NO_TAG, // red
+    NO_TAG, // green larson animation
     ALIGNING_TO_TAG, // yellow
     PARTY_MODE, // rainbow 🌈
     OFF
-  }
+  };
 
   public Lights() {
 
@@ -32,9 +40,8 @@ public class Lights extends SubsystemBase {
 
     // array of integers that represents the R, G, B values of each lightcode enum
     lightOptionsMap = new TreeMap<LightCode, Integer[]>();
-    lightOptionsMap.put(LightCode.GAME_OBJECT, new Integer[] {0, 0, 255});
-    lightOptionsMap.put(LightCode.READY_TO_SCORE, new Integer[] {0, 255, 0});
-    lightOptionsMap.put(LightCode.NO_TAG, new Integer[] {255, 0, 0});
+    lightOptionsMap.put(LightCode.GAME_OBJECT, new Integer[] {0, 255, 0});
+    lightOptionsMap.put(LightCode.READY_TO_SCORE, new Integer[] {0, 0, 255});
     lightOptionsMap.put(LightCode.ALIGNING_TO_TAG, new Integer[] {255, 255, 0});
     lightOptionsMap.put(LightCode.OFF, new Integer[] {0, 0, 0});
   }
@@ -42,27 +49,46 @@ public class Lights extends SubsystemBase {
   public void toggleCode(LightCode light) {
     if (currentLightStatus == light) {
       currentLightStatus = LightCode.OFF;
-      setLEDs();
-    } else if (currentLightStatus == LightCode.PARTY_MODE) {
-      setPartyMode();
     } else {
       currentLightStatus = light;
-      setLEDs();
     }
+    setLEDs();
   }
 
   public void setLEDs() {
-    candle.setLEDs(
+    if (currentLightStatus != LightCode.PARTY_MODE) {
+      candle.setLEDs(
         lightOptionsMap.get(currentLightStatus)[0],
         lightOptionsMap.get(currentLightStatus)[1],
-        lightOptionsMap.get(currentLightStatus)[2]);
+        lightOptionsMap.get(currentLightStatus)[2]
+      );
+    } else {
+      setPartyMode();
+    }
   }
 
+  /** Party Mode is an animation that switches between all the colors of the rainbow */
   public void setPartyMode() {
     RainbowAnimation rainbowAnim = new RainbowAnimation(1, 0.5, Constants.NUM_CANDLE_LEDS);
     candle.animate(rainbowAnim);
   }
 
+  /**  */
   @Override
-  public void periodic() {}
+  public void periodic() {
+    if (currentLightStatus == LightCode.NO_TAG) {
+      if (blinkingTimer >= blinkingPeriod) {
+        if (blinkingColorOn) {
+          candle.setLEDs(0, 255, 0);
+        } else {
+          candle.setLEDs(0, 0, 0);
+        }
+        blinkingColorOn = !blinkingColorOn;
+        blinkingTimer = 0;
+      }
+      blinkingTimer++;
+    } else {
+      blinkingTimer = 0;
+    }
+  }
 }
