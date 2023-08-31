0029f5c0b26052334829eff1a5a2a776d608fca6
Pxndah <gujjasidharth@gmail.com>
2023-08-31 14:02:14 -0500

    Created NO_TAG animation
    * Made NO_TAG an animation in replacement of DEFAULT light code
    * removed periodic code
    * changed conditional in setLEDs to be more streamlined

--- src/main/java/frc/robot/subsystems/Lights.java
+++ src/main/java/frc/robot/subsystems/Lights.java
@@ -4,6 +4,8 @@ import com.ctre.phoenix.led.CANdle;
 import com.ctre.phoenix.led.CANdle.LEDStripType;
 import com.ctre.phoenix.led.CANdleConfiguration;
 import com.ctre.phoenix.led.RainbowAnimation;
+import com.ctre.phoenix.led.StrobeAnimation;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.RobotMap;
@@ -14,19 +16,12 @@ public class Lights extends SubsystemBase {
   private CANdle candle = new CANdle(RobotMap.CANDLE_CAN_ID);
   private CANdleConfiguration config = new CANdleConfiguration();
   private LightCode currentLightStatus = LightCode.OFF;
-  
-  /** Timer for the Blinking LED */
-  private int blinkingTimer = 0;
-  /** When the blinkingTimer reaches 10 (blinkingPeriod), the LED is toggled */
-  private int blinkingPeriod = 10;
-  /** If LEDs are currently blinking, then True would mean color is currently showing */
-  private boolean blinkingColorOn = false;
 
 
   public enum LightCode {
     GAME_OBJECT, // green
     READY_TO_SCORE, // blue
-    NO_TAG, // green larson animation
+    NO_TAG, // blinking green animation
     ALIGNING_TO_TAG, // yellow
     PARTY_MODE, // rainbow 🌈
     OFF
@@ -56,39 +51,31 @@ public class Lights extends SubsystemBase {
   }
 
   public void setLEDs() {
-    if (currentLightStatus != LightCode.PARTY_MODE) {
+    if (currentLightStatus == LightCode.PARTY_MODE) {
+      setPartyMode();
+    } else if (currentLightStatus == LightCode.NO_TAG) {
+      setNoTag();
+    } else {
       candle.setLEDs(
         lightOptionsMap.get(currentLightStatus)[0],
         lightOptionsMap.get(currentLightStatus)[1],
         lightOptionsMap.get(currentLightStatus)[2]
       );
-    } else {
-      setPartyMode();
     }
   }
+
+  /** No Tag is an animation that switches between no color and green */
+  public void setNoTag() {
+    StrobeAnimation strobeAnim = new StrobeAnimation(0, 255, 0);
+    candle.animate(strobeAnim);
+  };
 
   /** Party Mode is an animation that switches between all the colors of the rainbow */
   public void setPartyMode() {
     RainbowAnimation rainbowAnim = new RainbowAnimation(1, 0.5, Constants.NUM_CANDLE_LEDS);
     candle.animate(rainbowAnim);
-  }
+  };
 
-  /**  */
   @Override
-  public void periodic() {
-    if (currentLightStatus == LightCode.NO_TAG) {
-      if (blinkingTimer >= blinkingPeriod) {
-        if (blinkingColorOn) {
-          candle.setLEDs(0, 255, 0);
-        } else {
-          candle.setLEDs(0, 0, 0);
-        }
-        blinkingColorOn = !blinkingColorOn;
-        blinkingTimer = 0;
-      }
-      blinkingTimer++;
-    } else {
-      blinkingTimer = 0;
-    }
-  }
+  public void periodic() {}
 }
