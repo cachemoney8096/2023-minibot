fc41596c7f33070abe802cdae7b8d7c709b02e0a
avakimc <acohen1@ucls.uchicago.edu>
2023-09-23 15:39:40 -0500

    Added IntakeSequence and InterruptibleIntakeSequence
    - added intake sequential command
    - added an interruptible option

---
+++ src/main/java/frc/robot/commands/IntakeSequence.java
@@ -0,0 +1,35 @@
+package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.robot.subsystems.Lights;
+import frc.robot.subsystems.Lights.LightCode;
+import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.arm.Arm.ArmPosition;
+import frc.robot.subsystems.grabber.Grabber;
+
+public class IntakeSequence extends SequentialCommandGroup {
+  public IntakeSequence(Arm arm, Grabber grabber, Lights lights) {
+    addRequirements(arm, grabber, lights);
+    addCommands(
+        new InstantCommand(() -> lights.toggleCode(LightCode.OFF)),
+        new InstantCommand(() -> arm.setDesiredPosition(ArmPosition.INTAKE)),
+        new WaitUntilCommand(arm::atDesiredArmPosition),
+        new InstantCommand(() -> grabber.intake()),
+        new WaitUntilCommand((grabber::seeGamePiece)),
+        new InstantCommand(() -> lights.toggleCode(LightCode.GAME_OBJECT)),
+        new InstantCommand(() -> grabber.stopIntakingGamePiece()),
+        new InstantCommand(() -> arm.setDesiredPosition(ArmPosition.STARTING)));
+  }
+
+  public static Command interruptibleIntakeSequence(Arm arm, Grabber grabber, Lights lights) {
+    return new IntakeSequence(arm, grabber, lights)
+        .finallyDo(
+            (boolean interrupted) -> {
+              arm.setDesiredPosition(ArmPosition.STARTING);
+              grabber.stopIntakingGamePiece();
+              lights.toggleCode(LightCode.OFF);
+            });
+  }
+}
