f66720deeab90d20f9f7f07050c7825965c296dd
voidSevenSevenSix <86328356+voidSevenSevenSix@users.noreply.github.com>
2023-09-23 14:52:44 -0500

    Added code to send sensor info to shuffleboard

--- src/main/java/frc/robot/subsystems/grabber/Grabber.java
+++ src/main/java/frc/robot/subsystems/grabber/Grabber.java
@@ -4,6 +4,7 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
@@ -15,6 +16,8 @@ public class Grabber extends SubsystemBase {
   private CANSparkMax backMotor =
       new CANSparkMax(RobotMap.BACK_INTAKE_ROLLER_MOTOR_CAN_ID, MotorType.kBrushless);
 
+  private final DigitalInput gamePieceSensor = new DigitalInput(RobotMap.LIFT_GAME_PIECE_DIO);
+  
   public void spinMotors(double power) {
     frontMotor.set(power);
     backMotor.set(power);
@@ -44,11 +47,17 @@ public class Grabber extends SubsystemBase {
     spinMotors(0.0);
   }
 
+  public boolean seeGamePiece(){
+    boolean seeGamePieceNow = !gamePieceSensor.get();
+    return seeGamePieceNow;
+  }
+
   @Override
   public void initSendable(SendableBuilder builder) {
     super.initSendable(builder);
     builder.addDoubleProperty("Front Motor Set Speed", frontMotor::get, frontMotor::set);
     builder.addDoubleProperty("Back Motor Set Speed", backMotor::get, backMotor::set);
+    builder.addBooleanProperty("Sensor sees game piece", this::seeGamePiece, null);
   }
   public void periodic() {}
 }
