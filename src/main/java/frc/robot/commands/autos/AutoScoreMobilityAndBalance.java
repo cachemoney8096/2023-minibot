7a52db2560fa2f59dc087ae22e81a8e679a6df2e
Sid <79818750+Panda8188@users.noreply.github.com>
2023-10-18 18:43:05 -0500

    Bug fixes
    * Renamed AutoScoreOneAndBalance to AutoScoreMobilityAndBalance
    * Changed comment
    * Removed unncessary imports

--- src/main/java/frc/robot/commands/autos/AutoScoreMobilityAndBalance.java
+++ src/main/java/frc/robot/commands/autos/AutoScoreMobilityAndBalance.java
@@ -1,9 +1,6 @@
 package frc.robot.commands.autos;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.Calibrations;
-import frc.robot.commands.autos.components.AutoChargeStationBalance;
 import frc.robot.commands.autos.components.AutoMobilityChargeStationSequence;
 import frc.robot.commands.autos.components.AutoScoreOne;
 import frc.robot.subsystems.Lights;
@@ -11,14 +8,14 @@ import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.drive.DriveSubsystem;
 import frc.robot.subsystems.grabber.Grabber;
 
-public class AutoScoreOneAndBalance extends SequentialCommandGroup {
+/* Scores the initial game piece, drives over (mobility), comes back and balances */
+public class AutoScoreMobilityAndBalance extends SequentialCommandGroup {
     public static final double DISTANCE_UP_CHARGE_STATION_METERS = 4.5;
     public static final double DISTANCE_BACK_CHARGE_STATION_METERS = 3.15;
-    public AutoScoreOneAndBalance(Arm arm, Grabber grabber, Lights lights, DriveSubsystem drive) {
+    public AutoScoreMobilityAndBalance(Arm arm, Grabber grabber, Lights lights, DriveSubsystem drive) {
         addCommands(
             new AutoScoreOne(true, arm, grabber, lights),
-            /* assume we get mobility in addition to balancing + score 1 */
             new AutoMobilityChargeStationSequence(drive, DISTANCE_UP_CHARGE_STATION_METERS, DISTANCE_BACK_CHARGE_STATION_METERS)
-        );
+        );  
     }
 }
