17ff8fae33088b95b0a6c43d58480f6006630718
avakimc <acohen1@ucls.uchicago.edu>
2023-10-18 20:27:18 -0500

    Revisions to AutoScoreTwo
    - changed AutoScoreOne to be able to score at different heights
    - adjusted the path
    - added backup incase the robot fails to intake game piece

--- src/main/java/frc/robot/commands/autos/components/AutoScoreOne.java
+++ src/main/java/frc/robot/commands/autos/components/AutoScoreOne.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.autos.components;
 
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
@@ -17,13 +18,18 @@ public class AutoScoreOne extends SequentialCommandGroup {
   public AutoScoreOne(boolean fast, Arm arm, Grabber grabber, Lights lights) {
     addCommands(
         new InstantCommand(() -> arm.startScore()),
-        new WaitUntilCommand(() -> arm.atPosition(ArmPosition.SCORE_MID_HIGH))
-            .withTimeout(ArmCal.START_TO_PRESCORE_HIGH_SEC),
+        new ConditionalCommand(
+          new WaitUntilCommand(() -> arm.atPosition(ArmPosition.SCORE_MID_HIGH)).withTimeout(ArmCal.START_TO_PRESCORE_MID_HIGH_SEC),
+          new WaitUntilCommand(() -> arm.atPosition(ArmPosition.SCORE_LOW)).withTimeout(ArmCal.START_TO_PRESCORE_LOW_SEC),
+            () -> {
+              return arm.getScoreHeight() == ScoreHeight.HIGH
+                  || arm.getScoreHeight() == ScoreHeight.MID;
+            }),
         new InstantCommand(
             () -> {
               lights.toggleCode(LightCode.READY_TO_SCORE);
             }),
-        new InstantCommand(() -> grabber.score(ScoreHeight.HIGH)),
+        new InstantCommand(() -> grabber.score(arm.getScoreHeight())),
         new WaitCommand(GrabberCalibrations.EJECTION_WAIT_TIME),
         new InstantCommand(() -> arm.goToPosition(ArmPosition.STARTING)),
         new WaitUntilCommand(() -> arm.atPosition(ArmPosition.STARTING))
