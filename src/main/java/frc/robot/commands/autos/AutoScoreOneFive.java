d0cdcc3945dfd058215adff649ab0ce738cf4a1e
Sid <79818750+Panda8188@users.noreply.github.com>
2023-10-18 20:23:09 -0500

    bug fixes

--- src/main/java/frc/robot/commands/autos/AutoScoreOneFive.java
+++ src/main/java/frc/robot/commands/autos/AutoScoreOneFive.java
@@ -1,44 +1,38 @@
 package frc.robot.commands.autos;
 
-import java.util.HashMap;
-
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.commands.FollowPathWithEvents;
-
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.robot.commands.IntakeSequence;
+import frc.robot.commands.autos.components.*;
 import frc.robot.subsystems.Lights;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.drive.DriveCal;
 import frc.robot.subsystems.drive.DriveSubsystem;
 import frc.robot.subsystems.grabber.Grabber;
-import frc.robot.subsystems.grabber.GrabberCalibrations;
-import frc.robot.commands.autos.components.*;
+import java.util.HashMap;
 
 public class AutoScoreOneFive extends SequentialCommandGroup {
-    private PathPlannerTrajectory oneFive = 
-        PathPlanner.loadPath(
-            "OneFive", 
-            new PathConstraints(
-                DriveCal.MAX_LINEAR_SPEED_METERS_PER_SEC, 
-                DriveCal.MAX_LINEAR_ACCELERATION_METERS_PER_SEC_SQ
-            ));
+  private PathPlannerTrajectory oneFive =
+      PathPlanner.loadPath(
+          "OneFive",
+          new PathConstraints(
+              DriveCal.MAX_LINEAR_SPEED_METERS_PER_SEC,
+              DriveCal.MAX_LINEAR_ACCELERATION_METERS_PER_SEC_SQ));
 
-    private HashMap<String, Command> eventMap = new HashMap<>();
-    /* To-do: put events into event map  */
-    public AutoScoreOneFive(DriveSubsystem drive, Arm arm, Grabber grabber, Lights lights) {
-        addCommands(
-            new AutoScoreOne(false, arm, grabber, lights),
-            new FollowPathWithEvents(drive.followTrajectoryCommand(oneFive, true), oneFive.getMarkers(), eventMap)
-                .withTimeout(2.56),
-            new InstantCommand(() -> grabber.intake()),
-            new WaitUntilCommand(()->grabber.seeGamePiece()),
-            new InstantCommand(() -> grabber.spinMotors(GrabberCalibrations.HOLD_GAME_OBJECT_POWER))
+  private HashMap<String, Command> eventMap = new HashMap<>();
+  /* To-do: put events into event map  */
+  public AutoScoreOneFive(DriveSubsystem drive, Arm arm, Grabber grabber, Lights lights) {
+    eventMap.put(
+        "intakeGamePiece", IntakeSequence.interruptibleIntakeSequence(arm, grabber, lights));
+    addCommands(
+        new AutoScoreOne(false, arm, grabber, lights),
+        new FollowPathWithEvents(
+                drive.followTrajectoryCommand(oneFive, true), oneFive.getMarkers(), eventMap)
+            .withTimeout(2.56)
         );
-    }
-
+  }
 }
